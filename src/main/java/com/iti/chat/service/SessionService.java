8df63b80d9357dcf7a6c78339d5eb2c0b363e312
RehamAboelyazied7 <rehamaboelyazied@gmail.com>
2020-02-13 11:12:15 +0200

    added updateUser to userDAO

--- src/main/java/com/iti/chat/service/SessionService.java
+++ src/main/java/com/iti/chat/service/SessionService.java
@@ -10,4 +10,5 @@ public interface SessionService extends Remote {
     User login(String phone, String password, ClientService client) throws RemoteException, SQLException;
     void logout(User user) throws RemoteException;
     ClientService getClient(User user) throws RemoteException;
+    void updateInfo(User user) throws RemoteException;
 }
