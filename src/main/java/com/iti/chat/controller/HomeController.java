614c0c120710a8d1ae526a109b4e3584bd14178f
RehamAboelyazied7 <rehamaboelyazied@gmail.com>
2020-02-16 22:02:31 +0200

    updated GUI

--- src/main/java/com/iti/chat/controller/HomeController.java
+++ src/main/java/com/iti/chat/controller/HomeController.java
@@ -39,81 +39,11 @@ public class HomeController implements Initializable {
     @FXML
     AnchorPane container;
 
-    StatisticsDAOImpl dao = StatisticsDAOImpl.getInstance();
-    Map<String, Integer> countryMap = dao.countriesStats();
-    Map<Integer, Integer> genderMap = dao.genderStats();
 
-    public BarChart barCountry(Map<String, Integer> aMap) {
-        System.out.println(aMap);
-        CategoryAxis xAxis = new CategoryAxis();
-        xAxis.setLabel("Countries");
-        NumberAxis yAxis = new NumberAxis();
-        yAxis.setLabel("No of Users");
-        BarChart barChart = new BarChart(xAxis, yAxis);
-        barChart.setTitle("Population of Countries");
-        XYChart.Series<String, Number> series1 = new XYChart.Series<>();
-        countryMap.forEach((key, value) -> {
-
-            series1.getData().add(new XYChart.Data(key, value));
-
-        });
-        barChart.getData().addAll(series1);
-        return barChart;
-
-    }
-
-    public PieChart makePieFemaleMale(int male, int female) {
-
-        PieChart pie = new PieChart();
-        ObservableList<PieChart.Data> data = FXCollections.observableArrayList();
-        data.addAll(new PieChart.Data("Male", male),
-                new PieChart.Data("Female", female)
-        );
-        pie.setData(data);
-        pie.setTitle("Females and Males");
-        int sum = male + female;
-        for (PieChart.Data d : pie.getData()) {
-            Node slice = d.getNode();
-            double Precent = (d.getPieValue() / sum) * 100;
-            String tip = d.getName() + " = " + String.format("%.2f", Precent) + "%";
-            Tooltip tt = new Tooltip(tip);
-            Tooltip.install(slice, tt);
-        }
-        return pie;
-    }
-
-    public PieChart makeOnlineOffline(double online, double offline) {
-        PieChart pie = new PieChart();
-        ObservableList<PieChart.Data> data = FXCollections.observableArrayList();
-        data.addAll(new PieChart.Data("Online", online),
-                new PieChart.Data("Offline", offline)
-        );
-        pie.setData(data);
-        pie.setTitle("Online And Offline");
-        double sum = online + offline;
-        for (PieChart.Data d : pie.getData()) {
-            Node slice = d.getNode();
-            double Precent = (d.getPieValue() / sum) * 100;
-            String tip = d.getName() + " = " + String.format("%.2f", Precent) + "%";
-            Tooltip tt = new Tooltip(tip);
-            Tooltip.install(slice, tt);
-        }
-        return pie;
-    }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        int males = genderMap.getOrDefault(Gender.MALE, 0);
-        int females = genderMap.getOrDefault(Gender.FEMALE, 0);
-        int allCount = dao.allUsersCount();
-        int online = 0;
-        int offline = 0;
-        try {
-            online = SessionServiceProvider.getInstance().onlineUsers();
-            offline = allCount - online;
-        } catch (RemoteException ex) {
-            Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
-        }
+
         adminbarController.serviceButton.setOnAction(ae -> {
             try {
                 FXMLLoader loader = new FXMLLoader();
@@ -131,7 +61,6 @@ public class HomeController implements Initializable {
                 loader.setLocation(HomeController.class.getResource("/fxml/Announcment.fxml"));
                 Parent parent = loader.load();
                 container.getChildren().clear();
-
                 container.getChildren().add(parent);
             } catch (IOException ex) {
                 Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
@@ -141,7 +70,7 @@ public class HomeController implements Initializable {
         adminbarController.StatsButton.setOnAction(ae -> {
             try {
                 FXMLLoader loader = new FXMLLoader();
-                loader.setLocation(HomeController.class.getResource("/fxml/Service.fxml"));
+                loader.setLocation(HomeController.class.getResource("/fxml/statisticsScene.fxml"));
                 Parent parent = loader.load();
                 container.getChildren().clear();
                 container.getChildren().add(parent);
@@ -149,6 +78,7 @@ public class HomeController implements Initializable {
                 Logger.getLogger(HomeController.class.getName()).log(Level.SEVERE, null, ex);
             }
         });
+
 //        root.getChildren().add(makePieFemaleMale(males, females));
 //        root.getChildren().add(makeOnlineOffline(online, offline));
 //        root.getChildren().add(barCountry(countryMap));
